8f774f56adc673c6a78768656841a4b182ab45e1
oborgroup <tech@oborgroup.net>
2026-06-02 04:34:56 +0000

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/zglobal_v5_mobile.html"
+      style={{
+        border: "none",
+        width: "100%",
+        height: "100vh",
+      }}
+    />
+  );
+}
